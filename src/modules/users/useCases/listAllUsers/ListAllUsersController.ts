07e13ca843cd0aea40dfda38751fdc61b7eba060
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-07 12:19:43 -0300

    refactor: removes any type

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,8 +6,12 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id }: any = request.headers;
+    const { user_id: id } = request.headers;
+
+    const user_id = String(id);
+
     const users = this.listAllUsersUseCase.execute({ user_id });
+
     return response.status(200).json(users);
   }
 }
