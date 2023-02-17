d588ffb77940264958ff808fef37e2a99d50652b
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-17 12:40:58 -0300

    feat: valid if user exists when list all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,6 +9,10 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("Usuário não existe!");
+
     return this.usersRepository.list();
   }
 }
