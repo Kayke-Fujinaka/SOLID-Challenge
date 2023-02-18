e944dfcd4ba83b38365be5a0db3508e53501e3ec
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-18 17:35:09 -0300

    feat: valid if not found a user

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -1,3 +1,4 @@
+import { HttpError } from "../../../../shared/errors/HttpError";
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -10,6 +11,9 @@ class TurnUserAdminUseCase {
 
   execute({ user_id }: IRequest): User {
     const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new HttpError("User not found", 404);
+
     return this.usersRepository.turnAdmin(user);
   }
 }
