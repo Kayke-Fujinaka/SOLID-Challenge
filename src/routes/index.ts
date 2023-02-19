5681998821ab2ca98fcead647f4fb4b867a94e36
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-02-19 16:48:52 -0300

    config: swagger integration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "../swagger.json";
 import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/users", usersRoutes);
+
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerFile));
 
 export { router };
